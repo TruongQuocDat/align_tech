ed40806dc332351aeb5ccd5b165a6e0ad17e1cd2
Dat Truong Quoc <kiljeaden277@gmail.com>
2021-04-08 23:51:35 +0700

    Add gem devise, create controller, model, layout for admin, user (#1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
+  devise_for :admins, only: %i[session], controllers: { sessions: 'admins/sessions' }
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  namespace :admins do
+    root 'dashboard#index'
+  end
 end
